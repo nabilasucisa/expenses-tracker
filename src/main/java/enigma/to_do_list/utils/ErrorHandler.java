300c06224244cf398d50158680b578b5f37dc58a
nabilasucisa <nabila.s.syabani@gmail.com>
2024-08-01 23:00:22 +0700

    #2 add error handler

--- src/main/java/enigma/to_do_list/utils/ErrorHandler.java
+++ src/main/java/enigma/to_do_list/utils/ErrorHandler.java
@@ -2,53 +2,53 @@ package enigma.to_do_list.utils;
 
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.UnexpectedTypeException;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException e) {
-        String message = e.getMessage();
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-//        if (e.getMessage().contains("name")) {
-//            message = "name cannot be blank";
-//        } else if (e.getMessage().contains("balance")) {
-//            message = "balance cannot be null";
-//        }
-
-        return Response.renderJson2(message, status);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String message = "Invalid input data!";
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-
-    @ExceptionHandler(UnexpectedTypeException.class)
-    public ResponseEntity<?> handleUnexpectedType(UnexpectedTypeException e) {
-        String message = e.getMessage();
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-
-//        if (e.getMessage().contains("name")) {
-//            message = "name cannot be blank";
-//        }
-        return Response.renderJson2(message, status);
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoHandlerFound(NoHandlerFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), "Resource not found!");
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntime(RuntimeException e) {
-        String message = e.getMessage();
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.value(), "You don't have permission to access this!");
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
 
-//        if (e.getMessage().contains("user with id")) {
-//            message = "User not found";
-//        } else if (e.getMessage().contains("car with id")) {
-//            message = "Car not found";
-//        } else if (e.getMessage().contains("brand with id")) {
-//            message = "Brand not found";
-//        } else if (e.getMessage().contains("rent with id")) {
-//            message = "Rent not found";
-//        }
-        return Response.renderJson2(message, status);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unexpected error occurred! Server error!");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Getter
+    @Setter
+    public static class ErrorResponse {
+        private int status;
+        private String message;
+
+        public ErrorResponse(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
     }
 }
